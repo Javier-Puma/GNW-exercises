282cc5c7999fd328e60f08f05bfe2ae33e38cd07
JavierPuma-Cod <codigos.jp.2002@gmail.com>
2026-08-17 23:22:30 -0500

    GNW-exercises-04: creation of IRepositoryProducto

---
+++ crud/src/main/java/com/gnw/crud/repository/IProductoRepository.java
@@ -0,0 +1,10 @@
+package com.gnw.crud.repository;
+
+import com.gnw.crud.model.Producto;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface IProductoRepository extends JpaRepository<Producto, Long> {
+
+}
